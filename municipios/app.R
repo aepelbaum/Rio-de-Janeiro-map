92a10a1b06bbe9ea4917c9bd80e221ac40dc6dd4
Alexandre Epelbaum <ale_epe@yahoo.com.br>
2020-08-10 17:09:58 -0300

    Primeiro commit

--- municipios/app.R
+++ municipios/app.R
@@ -7,7 +7,6 @@
 #    http://shiny.rstudio.com/
 #
 
-library(flexdashboard)
 library(tidyverse)
 library(sf)
 library(rgdal)
@@ -18,12 +17,11 @@ library(ggthemes)
 library(shiny)
 
 df_rj <- read_municipality( code_muni = "RJ", year= 2018) %>% 
-    mutate(name_muni=str_replace_all(name_muni, " De "," de ")) %>% 
-    mutate(name_muni=str_replace_all(name_muni, " Do "," do ")) %>% 
-    mutate(name_muni=str_replace_all(name_muni, " Dos "," dos "))
+  mutate(name_muni=str_replace_all(name_muni, " De "," de ")) %>% 
+  mutate(name_muni=str_replace_all(name_muni, " Do "," do ")) %>% 
+  mutate(name_muni=str_replace_all(name_muni, " Dos "," dos "))
 
 df_municipios_rj <- df_rj$name_muni %>% fct_inorder()
-
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -31,21 +29,31 @@ ui <- fluidPage(
     # Application title
     titlePanel("Municípios do Rio de Janeiro"),
 
-    # Sidebar with a slider input for number of bins 
-    selectInput("municipio",
-                label = "Selecione o município")
+    sidebarLayout(
+      sidebarPanel(
+        selectInput("municipio",
+                    label = "Selecione o município",
+                    choices = df_municipios_rj, selected = "Rio de Janeiro")
+      ),
+      
+      # Show a plot of the generated distribution
+      mainPanel(
+        plotOutput("mapPlot")
+      )
+    )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-    # output$distPlot <- renderPlot({
-    #     df_rj %>% ggplot() +
-    #         geom_sf(color="#FEBF57", size=.15, show.legend = FALSE) +
-    #         theme_minimal() +
-    #         labs(x = NULL,
-    #              Y = NULL)
-    # })
+    output$mapPlot <- renderPlot({
+        df_rj %>% ggplot() +
+            geom_sf(color="#FEBF57", size=.15, show.legend = FALSE) +
+            geom_sf(fill = "red", data = df_rj %>% filter(name_muni == input$municipio)) + 
+            theme_minimal() +
+            labs(x = NULL,
+                 Y = NULL)
+    })
 }
 
 # Run the application 
