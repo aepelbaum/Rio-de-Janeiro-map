bc6aaf79117c0844d4953b63a03d8f84410092a8
Alexandre Epelbaum <ale_epe@yahoo.com.br>
2020-08-13 11:26:31 -0300

    Mapas com os índices dos municípios do Rio de Janeiro

--- indices/app.R
+++ indices/app.R
@@ -12,12 +12,20 @@ library(tidyverse)
 library(geobr)
 library(shiny)
 
+#Lê CSV com indices dos municípios do Rio de Janeiro
+df_indice <- read_csv2("../data/dados-rj.csv", col_names = TRUE,
+                       locale = locale(encoding = "ISO-8859-1"), col_types = NULL) %>% 
+  rename(code_muni = codigo_municipio)
+
 df_rj <- read_municipality( code_muni = "RJ", year= 2018) %>% 
   mutate(name_muni=str_replace_all(name_muni, " De "," de ")) %>% 
   mutate(name_muni=str_replace_all(name_muni, " Do "," do ")) %>% 
   mutate(name_muni=str_replace_all(name_muni, " Dos "," dos "))
 
 df_municipios_rj <- df_rj$name_muni %>% fct_inorder()
+
+df_rj_indice <- df_rj %>% 
+  left_join(df_indice, by = "code_muni")
 
 # Define UI for application that draws a map
 ui <- fluidPage(
@@ -48,10 +56,13 @@ server <- function(input, output) {
 
     output$drawMap <- renderPlot({
         df_rj %>% ggplot() +
-            geom_sf(size=.15, show.legend = FALSE) +
-            geom_sf(fill = "blue", data = df_rj %>% filter(name_muni == input$municipio)) + 
-            theme_minimal() +
-            coord_sf(datum = NA)
+                  geom_sf(data=df_rj_indice, aes(fill=area), color= "black", size=.15) +
+                  labs(subtitle="População dos municípios do RIo de Janeiro", size=8) +
+                  # scale_fill_distiller(palette = "Blues", name="População", limits = c(min(df_rj_indice$populacao),max(df_rj_indice$populacao))) +
+                  #  scale_fill_continuous(name = "Área", label = scales::comma) +
+                  scale_fill_continuous(low = "white", high = "red",name = "Área", label = scales::comma) +
+                  theme_minimal() #+
+                  #no_axis
     })
 }
 
